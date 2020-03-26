b008366b72705cc9288520add1feb48556c5c60d
Ildana <ildana.ruzybayeva@gmail.com>
2020-03-26 09:02:15 +0000

    bodyparser to handle post

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const expressGraphQl = require("express-graphql");
 const { GraphQLSchema } = graphql;
 const { query } = require("./schemas/queries");
 const { mutation } = require("./schemas/mutations");
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser())
 
 const { Client } = require('pg')
 const client = new Client({
@@ -18,14 +24,36 @@ const schema = new GraphQLSchema({
   mutation
 });
 
-var app = express();
+
 app.use(
-  '/',
+  '/graphql',
   expressGraphQl({
     schema: schema,
     graphiql: true
   })
 );
+
+
+app.get("/", async (request, res) => {
+    try {
+        const sql = "SELECT * FROM users";
+        const result = await client.query(sql);
+        res.send(result.rows);
+    } catch(err) {
+        console.log(err)
+    }
+});
+
+app.post("/", async (request, res) => {
+    try {
+        const sql = "INSERT INTO users (username, email) VALUES ($1, $2);";
+        const values = [request.body.username, request.body.email];
+        const result = await client.query(sql, values);
+        res.send(result);
+    } catch(err) {
+        console.log(err)
+    }
+});
 
 app.listen(3000, () =>
   console.log('GraphQL server running on localhost:3000')
